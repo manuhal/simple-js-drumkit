8f6efbcce53475fbd5ce4586071973ee8d35b59f
Manu <44656862+manuelhal@users.noreply.github.com>
2019-04-26 10:32:54 -0700

    rename script.js, cleanup comments

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ class Pad {
         aud.currentTime = 0;
         return aud.play();
     }
-
 }
 
 const a = new Pad('kick', './sounds/kick.mp3');
@@ -25,62 +24,31 @@ const l = new Pad('kick', './sounds/crash.mp3');
 const drumKit = { a, s, d, f, h, j, k, l }
 
 const drums = document.querySelectorAll('.pad');
-// console.log(drums)
 
 // added ev-listener to .drum class
 drums.forEach((item) => {
     item.addEventListener('click', () => {
-        // console.log('clicked', item.innerHTML[0])
         playPad(item.innerHTML[0].toLowerCase())
-
     })
 })
 
+
 //add keylistner to the doc
 document.addEventListener('keypress', (evt) => {
-    // console.log('key pressed, evt=', evt)
     playPad(evt.key.toLowerCase())
-
 })
 
 
 function playPad(padName) {
     if (padName in drumKit) {
         const activeButton = document.querySelector(`.${padName}`)
-        // console.log('activeButton', activeButton);
-
-        // console.log('playing pad ', padName);
         drumKit[padName].playSound();
 
+        //activate .pressed class then deactivate after  .2 sec
         activeButton.classList.add('pressed')
-
         setTimeout(() => {
             activeButton.classList.remove('pressed')
         }, 200)
-
     }
 }
-
-
-// use for..of, but kenapa ga bisa ya? 
-// for (item of document.querySelectorAll('.drum')) {
-//     item.addEventListener('click', () => {
-//         console.log(item)
-//     })
-//     // console.log('clicked ', item)
-// }
-
-
-// //use classic for loop
-// for (let i = 0; i < drums.length; i++) {
-//     drums[i].addEventListener('click', () => {
-//         console.log('drums[i]', drums[i])
-//     })
-// }
-
-
-// test - adding evt listnr
-// document.querySelector('button').addEventListener('click', () => {
-//     alert('u clicked me')
-// })
 
